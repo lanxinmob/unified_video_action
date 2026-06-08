f7d474826eacbb9147875443ca2ef02481f3c532
mobu <mobu54511@gmail.com>
2026-06-08 21:53:23 +0800

    fix reset_async

--- unified_video_action/gym_util/async_vector_env.py
+++ unified_video_action/gym_util/async_vector_env.py
@@ -186,7 +186,7 @@ class AsyncVectorEnv(VectorEnv):
         _, successes = zip(*[pipe.recv() for pipe in self.parent_pipes])
         self._raise_if_errors(successes)
 
-    def reset_async(self):
+    def reset_async(self, **kwargs):
         self._assert_is_running()
         if self._state != AsyncState.DEFAULT:
             raise AlreadyPendingCallError(
